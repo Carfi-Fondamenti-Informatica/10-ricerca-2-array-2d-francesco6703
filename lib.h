2064481a396749ae6b4bde917e905de8c91fe2cc
francesco6703 <127511152+francesco6703@users.noreply.github.com>
2023-06-01 18:42:03 +0200

    Update lib.h

--- lib.h
+++ lib.h
@@ -2,6 +2,10 @@ void init(char mat[], int n);
 
 void init(char mat[5][15]);
 
-void inserimento(char nomi[5][15], char cognomi[5][15]);
+void inserimento(char str[], int size);
+
+bool controllo1(char nomi[5][15], char nome[]);
+
+bool controllo2(char cognomi[5][15], char cognome[]);
 
 int ricerca(char nomi[5][15], char cognomi[5][15], char nome[], char cognome[]);
