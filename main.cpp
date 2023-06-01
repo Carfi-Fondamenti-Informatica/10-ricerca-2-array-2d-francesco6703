217c450914fcc0d384d66b6124c783e17a9f5094
francesco6703 <127511152+francesco6703@users.noreply.github.com>
2023-06-01 17:12:07 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
-
-int main(){
-  return 0;
+#include <cstring>
+#include "lib.h"
+int main() {
+    char nomi[5][15], cognomi[5][15];
+    init(nomi);
+    init(cognomi);
+    inserimento(nomi, cognomi);
+    
+    char nome[15], cognome[15];
+    cout << "Inserisci il nome da cercare: ";
+    cin.getline(nome, 15);
+    cout << "Inserisci il cognome da cercare: ";
+    cin.getline(cognome, 15);
+    
+    int id = ricerca(nomi, cognomi, nome, cognome);
+    if (id != -1) {
+        cout << "Trovato in posizione " << id << endl;
+    }
+    else {
+        cout << "Non trovato" << endl;
+    }
+    
+    return 0;
 }
