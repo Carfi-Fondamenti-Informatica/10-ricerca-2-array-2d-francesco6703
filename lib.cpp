7cd705050ccc8bb4f51975ec0bef3c13d3b77a7c
francesco6703 <127511152+francesco6703@users.noreply.github.com>
2023-06-08 22:24:54 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,37 +1,69 @@
 #include <iostream>
-#include <cstring>
 #include "lib.h"
+
 using namespace std;
 
-void init(char mat[], int n) {
-    for (int i = 0; i < n; i++) {
-        mat[i] = ' ';
+
+
+void init (char mat[], int n){
+    for(int i=0;i<n;i++){
+        mat[i]= ' ';
+    }
+}
+void init(char mat[5][15]){
+    for(int i=0;i<5;i++){
+        init(mat[i],15);
     }
 }
 
-void init(char mat[5][15]) {
-    for (int i = 0; i < 5; i++) {
-        init(mat[i], 15);
+void inserimento(char nomi[5][15], char cognomi[5][15]){
+    for(int i=0; i<5;i++){
+        inserimento(nomi[i],15);
+        inserimento(cognomi[i],15);
     }
 }
 
-void inserisci_stringa(char cstring[], int a) {
-    cin.getline(cstring, a);
-}
 
-void inserimento(char nomi[5][15], char cognomi[5][15]) {
-    for (int i = 0; i < 5; i++) {
-        inserisci_stringa(nomi[i], 15);
-        inserisci_stringa(cognomi[i], 15);
+
+void inserimento(char mat[], int a){
+    char n[a*10];
+    init(n,a*10);
+    //for(int i=0;i<(a*10);i++){
+    //   mat[i]=n[a*10];
+    // }
+
+    cin >> n;
+
+    for(int i=0;i<a;i++){
+        mat[i] =n[i];
     }
 }
 
-int ricerca(char nomi[5][15], char cognomi[5][15], char nome[], char cognome[]) {
-    for (int i = 0; i < 5; i++) {
-        if (strcmp(nomi[i], nome) == 0 && strcmp(cognomi[i], cognome) == 0) {
-            return i;
+
+int ricerca(char nomi[5][15], char cognomi[5][15], char nome[15], char cognome[15]){
+    for(int i=0;i<5;i++){
+            int j=0;
+
+
+        for(j=0;j<15;j++) {
+
+
+            if (cognomi[i][j] != cognome[j]) {
+
+                break;
+            }
+
+            if (nomi[i][j] != nome[j]) {
+
+                break;
+            }
+        }if(j==15) {
+          return i;
+
+
+    }
+
         }
-    }
+
     return -1;
 }
-
