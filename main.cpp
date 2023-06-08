5681227b7d73cacb69712975b2f352d2b7cc6600
francesco6703 <127511152+francesco6703@users.noreply.github.com>
2023-06-08 20:54:07 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
 #include "lib.h"
 using namespace std;
+
 int main() {
     char nomi[5][15], cognomi[5][15];
     init(nomi);
     init(cognomi);
     inserimento(nomi, cognomi);
-    
+
     char nome[15], cognome[15];
-    inserimentonome(nome, 15);
-    inserimentocognome(cognome, 15);
-    
+    inserisci_stringa(nome, 15);
+    inserisci_stringa(cognome, 15);
+    cout << endl;
+
     int id = ricerca(nomi, cognomi, nome, cognome);
     if (id != -1) {
         cout << "Trovato in posizione " << id << endl;
-    }
-    else {
+    } else {
         cout << "Non trovato" << endl;
     }
-    
+
     return 0;
 }
