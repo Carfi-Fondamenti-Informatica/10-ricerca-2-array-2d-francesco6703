4f5ce38bff71a5c756acab0267746fb1e53795ae
francesco6703 <127511152+francesco6703@users.noreply.github.com>
2023-06-08 22:36:38 +0200

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,5 +1,5 @@
-void init (char mat[], int n);
+void init(char mat[], int n);
 void init(char mat[5][15]);
 void inserimento(char nomi[5][15], char cognomi[5][15]);
 void inserimento(char mat[], int a);
-int ricerca(char nomi[5][15], char cognomi[5][15], char nome[15], char cognome[15]);
+int ricerca(char nomi[5][15], char cognomi[5][15], char nome[15], char cognome[15]);
